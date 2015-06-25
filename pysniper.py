aef7944783ad7a6fabbf11b4cad6c825f552a6d2
aims <jbf@zuloo.de>
2015-06-25 16:27:50 +0200

    switched to an even more modular concept of doing requests
    snipe.py and request/login.req are now deprecated and will be removed
    later

--- pysniper.py
+++ pysniper.py
@@ -1,4 +1,108 @@
+from __future__ import print_function
+
 import requests
+import warnings
+import json
+import re
 
-s=requests.session()
+from lxml import html
 
+# warnings.filterwarnings("ignore")
+
+def do_request(request_list, session=None, data=None, config=None):
+    post_data = data or {}
+    config = config or {}
+    # create session if none given
+    session = session or requests.Session()
+
+    def replace_conf_var(string):
+        def replace_variable(match):
+            return config.get(re.sub(r'[^a-zA-Z0-9]', '', match.group()), '')
+        return re.sub(r'{%.*?%}', replace_variable, string)
+
+    # cycle through request list
+    for req in request_list:
+        # if no url given continue
+        if not req.get('url'):
+            continue
+
+        print('{}: {}'.format(req.get('method','GET'),req.get('url')))
+
+        res = None
+
+        # update post_data with fields given in the request definition
+        if req.get('post_data'):
+            for key in req.get('post_data'):
+                post_data[key] = replace_conf_var(req['post_data'][key])
+
+        if req.get('debug'):
+            print(post_data)
+
+        # by default verify ssl certificates
+        verify_ssl = req.get('verify',True)
+        # if method post send 
+        if req.get('method') == "POST":
+            res = session.post(req.get('url'),data=post_data,verify=verify_ssl)
+        else:
+            res = session.post(req.get('url'),verify=verify_ssl)
+
+        if req.get('output'):
+            print(res.text)
+
+        # TODO check for response state and decide for continue or break
+
+        dom = html.fromstring(res.text)
+
+        # handle response - parse form
+        if req.get('parse_form'):
+            input_fields = dom.xpath(req.get('parse_form'))
+
+            # populate post data from xpath'ed input fields
+            post_data={}
+            for field in input_fields:
+                if field.get('name'):
+                    post_data[field.get('name')] = field.get('value', '')
+
+        stop = False
+        if req.get('tests'):
+            for test in req.get('tests'):
+
+                # find dom elements to check 
+                elements = dom.xpath(test.get('xpath'))
+
+                # if no dom elements found continue
+                if not elements:
+                    continue
+
+                # if no match clause just proceed
+                if not test.get('match'):
+                    print(test.get('message',''))
+                    if not test.get('continue'): stop = True
+                    continue
+
+                # replace config variable in match clause
+                needle = replace_conf_var(test.get('match',''))
+
+                # match needle against elements texts
+                regex = re.compile(needle)
+                for element in elements:
+                    if regex.match(element.text):
+                        print(test.get('message',''))
+                        if not test.get('continue'): stop = True
+        
+        if stop:
+            return session
+
+def load_json(file_name):
+    json_data = {}
+    try:
+        with open(file_name) as f:
+            json_data = json.loads(f.read())
+    except (OSError, IOError) as e:
+        print("Could not open file {}, {}:".format(file_name, e))
+    return json_data
+
+login_request = load_json("request/login.json")
+user_data = load_json("config/user.cfgg")
+
+do_request(login_request,config=user_data)
--- snipe.py
+++ snipe.py
@@ -3,9 +3,11 @@ import json
 
 from lxml import html
 
-def checkResponse(response,checklist):
+#def checkResponse(response,checklist):
 
-def login(session):
+def login():
+    session = requests.session()
+
     request_file = open("request/login.req")
     login_request = json.loads(request_file.read())
     request_file.close()
@@ -13,20 +15,40 @@ def login(session):
     config_file = open("config/user.cfg")
     user_data = json.loads(config_file.read())
     config_file.close()
+    
+    # get all the fancy ebay cookies
+    session.get(login_request['prefetch_url'],verify=False)
 
-    login_request["post_data"]["userid"] = user_data["user"]
-    login_request["post_data"]["pass"] = user_data["pass"]
-
-    referer = session.get(login_request['prefetch'])
-    page = session.get(login_request['url'])
-    dom = html.fromstring(page.text)
+    # get the login form and extract all fields
+    login_page = session.get(login_request['login_url'],verify=False)
+    dom = html.fromstring(login_page.text)
 
     input_fields = dom.xpath('//form[@name="SignInForm"]//input')
     
+    # populate post data for login
+    post_data={}
     for field in input_fields:
-        print(field.attrib['name'],': ',field.attrib['value'])
-    #session.post(login_request["url"],login_request["post_data"])
+        post_data[field.name] = field.value
+        # print(field.attrib['name'],': ',field.attrib['value'])
+
+    # change values if any given in request file
+    if login_request.get('post_data'):
+        for key in login_request['post_data']:
+            post_data[key] = login_request['post_data'][key]
+
+    # add login and password from user.cfg
+    post_data['userid'] = user_data['user']
+    post_data['pass'] = user_data['pass']
+
+    # do login
+    response = session.post(login_request["login_url"],data=post_data,verify=False)
+    print(response.text)
+    # do after login redirect (done with javascript in browsers)
+    my_ebay_page = session.get(login_request["after_login_url"])
+    
+
+
+    return session
     #return checkResponse(response,login_request["responses"])
 
-session = requests.session()
-login(session)
+session = login()
